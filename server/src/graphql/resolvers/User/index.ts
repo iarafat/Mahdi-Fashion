2e37cb64bfb2e4a58f811faa0651024bc778aa23
iarafat <miyasinarafat@gmail.com>
2020-08-21 15:13:39 +0600

    added user sign up api

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -1,6 +1,22 @@
 import {ObjectId} from 'mongodb';
 import {IResolvers} from 'apollo-server-express';
 import {Database, IUser, IUserAuth} from "../../../lib/types";
+import * as bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+
+const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 10)
+};
+
+const validatePassword = async (plainPassword: string, hashPassword: string) => {
+  return await bcrypt.compare(plainPassword, hashPassword);
+};
+
+const accessToken = (id: ObjectId) => {
+    const secret: any = process.env.JWT_SECRET;
+  return jwt.sign({UserId: id}, secret, {expiresIn: "1d"})
+};
+
 
 export const usersResolvers: IResolvers = {
     Query: {
@@ -18,20 +34,34 @@ export const usersResolvers: IResolvers = {
             _root: undefined,
             {phone, password}: { phone: string, password: string },
             {db}: { db: Database }
-        )/*: Promise<IUser>*/ => {
-            const user: IUser = {
-                _id: new ObjectId(),
-                password: password,
-                phones: [{number: phone}],
-                created_at: new Date().toTimeString(),
-            };
-            const userResult = await db.users.insertOne(user);
+        ): Promise<IUserAuth> => {
 
-            if (!userResult.ops) {
-                throw new Error("Failed to sign up");
+            const address = {
+                title: "",
+                address: "",
+                division: "",
+                district: "",
+                region: "",
+                is_primary: false,
             }
 
-            return userResult.ops;
+            const user: IUser = {
+                _id: new ObjectId(),
+                name: "",
+                email: "",
+                password: await hashPassword(password),
+                phones: [{number: phone, status: false, is_primary: false}],
+                delivery_address: [address],
+                created_at: new Date().toString(),
+            };
+
+
+            const insertResult = await db.users.insertOne(user);
+            const insertedUser = insertResult.ops[0];
+            return  {
+                user: insertedUser,
+                access_token: accessToken(insertedUser._id),
+            }
         }
     },
 
