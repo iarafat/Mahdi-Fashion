24d53963e67adae13caaa2bf9e0995675767b73c
noman abdulla <noman.fcibappy@gmail.com>
2020-11-02 10:27:49 +0600

    move

--- client/packages/shop/src/utils/apollo.js
+++ client/packages/shop/src/utils/apollo.js
@@ -11,6 +11,10 @@ function createApolloClient() {
     link: new HttpLink({
       uri: process.env.NEXT_PUBLIC_GRAPHQL_API_ENDPOINT, // Server URL (must be absolute)
       credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+      onError: ({ networkError, graphQLErrors }) => {
+        console.log('graphQLErrors', graphQLErrors);
+        console.log('networkError', networkError);
+      },
     }),
     cache: new InMemoryCache(),
   });
