77e7e32eaab4d030bf4f9a817c237f76f5e342e2
iarafat <miyasinarafat@gmail.com>
2020-11-10 23:14:17 +0600

    added delivery address crud and change password apis

--- server/src/graphql/resolvers/User/index.ts
+++ server/src/graphql/resolvers/User/index.ts
@@ -32,7 +32,7 @@ export const usersResolvers: IResolvers = {
         },
         getUser: async (
             _root: undefined,
-            {id}: { id: string},
+            {id}: { id: string },
             {db, req}: { db: Database, req: Request }
         ): Promise<IUser> => {
             await authorize(req, db);
@@ -53,22 +53,12 @@ export const usersResolvers: IResolvers = {
                 throw new Error("User already registered.");
             }
 
-            const address = {
-                title: "",
-                address: "",
-                division: "",
-                district: "",
-                region: "",
-                is_primary: false,
-            }
-
             const user: IUser = {
                 _id: new ObjectId(),
                 name: "",
                 email: "",
                 password: await hashPassword(password),
                 phones: [{number: phone, status: false, is_primary: true}],
-                delivery_address: [address],
                 created_at: new Date().toString(),
             };
 
@@ -101,7 +91,7 @@ export const usersResolvers: IResolvers = {
         },
         updateUserNameAndEmail: async (
             _root: undefined,
-            {id, name, email}: { id: string, name: string, email: string},
+            {id, name, email}: { id: string, name: string, email: string },
             {db}: { db: Database }
         ): Promise<ICommonMessageReturnType> => {
             const userResult = await db.users.findOne({_id: new ObjectId(id)});
@@ -114,8 +104,6 @@ export const usersResolvers: IResolvers = {
                 {$set: {name: name, email: email}}
             );
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             return {
                 status: true,
                 message: "Updated successfully."
@@ -123,7 +111,7 @@ export const usersResolvers: IResolvers = {
         },
         addPhoneNumber: async (
             _root: undefined,
-            {id, number}: { id: string, number: string},
+            {id, number}: { id: string, number: string },
             {db}: { db: Database }
         ): Promise<ICommonMessageReturnType> => {
             const userResult = await db.users.findOne({_id: new ObjectId(id)});
@@ -140,11 +128,9 @@ export const usersResolvers: IResolvers = {
 
             await db.users.updateOne(
                 {_id: new ObjectId(id)},
-                {$push: {phones: {number: number, status: false, is_primary: false} }}
+                {$push: {phones: {number: number, status: false, is_primary: false}}}
             );
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             return {
                 status: true,
                 message: "Added successfully."
@@ -152,7 +138,7 @@ export const usersResolvers: IResolvers = {
         },
         updatePhoneNumber: async (
             _root: undefined,
-            {id, index, number}: { id: string, index: number, number: string},
+            {id, index, number}: { id: string, index: number, number: string },
             {db}: { db: Database }
         ): Promise<ICommonMessageReturnType> => {
             const userResult = await db.users.findOne({_id: new ObjectId(id)});
@@ -169,8 +155,6 @@ export const usersResolvers: IResolvers = {
                 {$set: {"phones.$.number": number}}
             );
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             return {
                 status: true,
                 message: "Updated successfully."
@@ -178,7 +162,7 @@ export const usersResolvers: IResolvers = {
         },
         setPhoneNumberPrimary: async (
             _root: undefined,
-            {id, index}: { id: string, index: number},
+            {id, index}: { id: string, index: number },
             {db}: { db: Database }
         ): Promise<ICommonMessageReturnType> => {
             const userResult = await db.users.findOne({_id: new ObjectId(id)});
@@ -215,8 +199,6 @@ export const usersResolvers: IResolvers = {
                 {$set: {phones: numbers}}
             );
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             return {
                 status: true,
                 message: "Set successfully."
@@ -224,7 +206,7 @@ export const usersResolvers: IResolvers = {
         },
         deletePhoneNumber: async (
             _root: undefined,
-            {id, index}: { id: string, index: number},
+            {id, index}: { id: string, index: number },
             {db}: { db: Database }
         ): Promise<ICommonMessageReturnType> => {
             const userResult = await db.users.findOne({_id: new ObjectId(id)});
@@ -259,11 +241,213 @@ export const usersResolvers: IResolvers = {
                 {$set: {phones: numbers}}
             );
 
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
             return {
                 status: true,
                 message: "Deleted successfully."
+            };
+        },
+        addDeliveryAddress: async (
+            _root: undefined,
+            {id, title, address, division, district, region}: {
+                id: string,
+                title: string,
+                address: string,
+                division: string,
+                district: string,
+                region: string
+            },
+            {db}: { db: Database }
+        ): Promise<ICommonMessageReturnType> => {
+            const userResult = await db.users.findOne({_id: new ObjectId(id)});
+            if (!userResult) {
+                throw new Error("User dose not exits.");
+            }
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (userResult.delivery_address && userResult.delivery_address.length == 3) {
+                throw new Error("Already added three address. You are not allowed to add more than three address.");
+            }
+
+            const newAddress = {
+                title: title,
+                address: address,
+                division: division,
+                district: district,
+                region: region,
+                is_primary: false,
+            }
+
+            await db.users.updateOne(
+                {_id: new ObjectId(id)},
+                {$push: {delivery_address: newAddress}}
+            );
+
+            return {
+                status: true,
+                message: "Added successfully."
+            };
+        },
+        updateDeliveryAddress: async (
+            _root: undefined,
+            {id, index, title, address, division, district, region}: {
+                id: string,
+                index: number,
+                title: string,
+                address: string,
+                division: string,
+                district: string,
+                region: string
+            },
+            {db}: { db: Database }
+        ): Promise<ICommonMessageReturnType> => {
+            const userResult = await db.users.findOne({_id: new ObjectId(id)});
+            if (!userResult) {
+                throw new Error("User dose not exits.");
+            }
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            const {title: title1, address: address1, division: division1, district: district1, region: region1, is_primary} = userResult.delivery_address[index];
+
+            await db.users.updateOne(
+                {_id: new ObjectId(id), "delivery_address.title": title1},
+                {
+                    $set: {
+                        "delivery_address.$.title": title ? title : title1,
+                        "delivery_address.$.address": address ? address : address1,
+                        "delivery_address.$.division": division ? division : division1,
+                        "delivery_address.$.district": district ? district : district1,
+                        "delivery_address.$.region": region ? region : region1,
+                        "delivery_address.$.is_primary": is_primary,
+                    }
+                }
+            );
+
+            return {
+                status: true,
+                message: "Updated successfully."
+            };
+        },
+        setDeliveryAddressPrimary: async (
+            _root: undefined,
+            {id, index}: { id: string, index: number },
+            {db}: { db: Database }
+        ): Promise<ICommonMessageReturnType> => {
+            const userResult = await db.users.findOne({_id: new ObjectId(id)});
+            if (!userResult) {
+                throw new Error("User dose not exits.");
+            }
+
+            const addresses = [];
+            const userAddresses = userResult.delivery_address;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            for (let i = 0; i < userAddresses.length; i++) {
+                if (i == index) {
+                    if (userAddresses) {
+                        addresses.push({
+                            title: userAddresses[i].title,
+                            address: userAddresses[i].address,
+                            division: userAddresses[i].division,
+                            district: userAddresses[i].district,
+                            region: userAddresses[i].region,
+                            is_primary: true
+                        });
+                    }
+                } else {
+                    if (userAddresses) {
+                        addresses.push({
+                            title: userAddresses[i].title,
+                            address: userAddresses[i].address,
+                            division: userAddresses[i].division,
+                            district: userAddresses[i].district,
+                            region: userAddresses[i].region,
+                            is_primary: false
+                        });
+                    }
+                }
+            }
+
+            await db.users.updateOne(
+                {_id: new ObjectId(id)},
+                {$set: {delivery_address: addresses}}
+            );
+
+            return {
+                status: true,
+                message: "Set successfully."
+            };
+        },
+        deleteDeliveryAddress: async (
+            _root: undefined,
+            {id, index}: { id: string, index: number },
+            {db}: { db: Database }
+        ): Promise<ICommonMessageReturnType> => {
+            const userResult = await db.users.findOne({_id: new ObjectId(id)});
+            if (!userResult) {
+                throw new Error("User dose not exits.");
+            }
+
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            if (userResult.delivery_address.length == 1) {
+                throw new Error("You are not allowed to delete your address.");
+            }
+
+
+            const addresses = [];
+            const userAddresses = userResult.delivery_address;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            for (let i = 0; i < userAddresses.length; i++) {
+                if (i != index) {
+                    if (userAddresses) {
+                        addresses.push({
+                            title: userAddresses[i].title,
+                            address: userAddresses[i].address,
+                            division: userAddresses[i].division,
+                            district: userAddresses[i].district,
+                            region: userAddresses[i].region,
+                            is_primary: userAddresses[i].is_primary
+                        });
+                    }
+                }
+            }
+
+            await db.users.updateOne(
+                {_id: new ObjectId(id)},
+                {$set: {delivery_address: addresses}}
+            );
+
+            return {
+                status: true,
+                message: "Deleted successfully."
+            };
+        },
+        changePassword: async (
+            _root: undefined,
+            {id, old_password, new_password, confirm_password}: { id: string, old_password: string, new_password: string, confirm_password: string },
+            {db}: { db: Database }
+        ): Promise<ICommonMessageReturnType> => {
+            const userResult = await db.users.findOne({_id: new ObjectId(id)});
+            if (!userResult) {
+                throw new Error("User dose not exits.");
+            }
+
+            const validatePass = await validatePassword(old_password, userResult.password);
+            if (!validatePass || new_password != confirm_password) {
+                throw new Error("Password dose not match.")
+            }
+
+            await db.users.updateOne(
+                {_id: new ObjectId(id)},
+                {$set: {password: await hashPassword(new_password)}}
+            );
+
+            return {
+                status: true,
+                message: "Changed successfully."
             };
         },
     },
