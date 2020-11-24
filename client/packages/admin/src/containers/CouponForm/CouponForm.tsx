262a2281044b5a5efd1ea2bd70cd25f37cda9397
iarafat <miyasinarafat@gmail.com>
2020-11-25 04:25:25 +0600

    fixed coupons admin issues

--- client/packages/admin/src/containers/CouponForm/CouponForm.tsx
+++ client/packages/admin/src/containers/CouponForm/CouponForm.tsx
@@ -193,7 +193,7 @@ const AddCoupon: React.FC<Props> = props => {
                   />
                 </FormFields>
 
-                <div data-baseweb="block" className="ai ae bk gs gt gu gv">
+                <div data-baseweb="block" className="ai ae bk gs gu gv">
                   <label className="bs dt bu bv gw b9">Expiration Date</label>
                   <div data-baseweb="input" className="bs bt cq cr cs ae ai">
                     <div data-baseweb="base-input" className="ct ae ai cu cv cw cx cy cz d0 d1 d2 d3 d4 d5 d6 d7 d8 bs bt cq cr cs d9 da db dc dd">
